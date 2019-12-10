db517b8c3553027c0a78d05bfd0aa092ee731639
tirtharajjana <tirtharajjana615@gmail.com>
2019-12-10 14:25:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+
+var bold=document.getElementById("bold");
+bold.onclick = function()
+{
+    document.execCommand("bold");
+}
+
+var bold=document.getElementById("italic");
+bold.onclick = function()
+{
+    document.execCommand("italic");
+}
+
+var bold=document.getElementById("underline");
+bold.onclick = function()
+{
+    document.execCommand("underline");
+}
+
+var bold=document.getElementById("sup");
+bold.onclick = function()
+{
+    document.execCommand("superscript");
+}
+
+
+var bold=document.getElementById("sub");
+bold.onclick = function()
+{
+    document.execCommand("subscript");
+}
+
+var bold=document.getElementById("left");
+bold.onclick = function()
+{
+    document.execCommand("justifyLeft");
+}
+
+var bold=document.getElementById("center");
+bold.onclick = function()
+{
+    document.execCommand("justifyCenter");
+}
+
+var bold=document.getElementById("right");
+bold.onclick = function()
+{
+    document.execCommand("justifyRight");
+}
+
+
+var bold=document.getElementById("del");
+bold.onclick = function()
+{
+    document.execCommand("delete");
+}
+
+
+var bold=document.getElementById("undo");
+bold.onclick = function()
+{
+    document.execCommand("undo");
+}
+
+var bold=document.getElementById("redo");
+bold.onclick = function()
+{
+    document.execCommand("redo");
+}
+
